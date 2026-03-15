182ac954043b1d18e75dff9a285780d37cf0961d
ketanpandit11 <240130510034.cs@gujaratvidyapith.org>
2026-03-15 11:05:53 +0530

    Add files via upload

---
+++ php/db,php.php
@@ -0,0 +1,20 @@
+<?php
+
+$servername = "localhost";
+$username   = "root";
+$password   = "";
+$dbname     = "ipl_users";
+
+
+$conn = mysqli_connect($servername, $username, $password, $dbname);
+
+if(!$conn)
+{
+    die("Database Connection Failed : " . mysqli_connect_error());
+}
+else
+{
+    echo "Database Connected Successfully";
+}
+
+?>
